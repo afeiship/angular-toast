3fb415ae86225631d5cf380d1a5a0fcd08e80b8e
afei <88603982@qq.com>
2016-05-07 08:18:44 +0800

    remove unused code

--- src/angular/main.js
+++ src/angular/main.js
@@ -2,8 +2,5 @@
 var ToastModule = angular.module('toast', []),
   extend = angular.extend,
   forEach = angular.forEach,
-  isDefined = angular.isDefined,
-  isNumber = angular.isNumber,
-  isString = angular.isString,
   jqLite = angular.element,
   noop = angular.noop;
