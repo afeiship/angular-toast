928f47f4d8af658ab881ea90d813f6e6226f430e
feizheng <1290657123@qq.com>
2016-07-27 16:18:23 +0800

    update angular-stiffener

--- dist/js/angular-toast.js
+++ dist/js/angular-toast.js
@@ -13,7 +13,7 @@
       return {
         restrict: 'E',
         transclude: true,
-        template: '<div id="widget-toast {{cssClass}}" class="ng-widget-toast" data-visible="{{visible}}" ng-bind-html="msg"></div>',
+        template: '<div class="nx-widget-toast {{cssClass}}" data-visible="{{visible}}" ng-bind-html="msg"></div>',
         scope: true
       };
     }]);
